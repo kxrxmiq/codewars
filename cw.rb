9bc7a97579e4346f5be7a7abfa89c8a07bde9961
kxrxmiq <qwerty1122k@mail.ru>
2025-10-06 12:35:17 +0300

    13

--- cw.rb
+++ cw.rb
@@ -65,3 +65,13 @@ end
 def shorten_to_date(long_date)
   long_date.split(",").first
 end
+
+#Find the area of the rectangle!
+def area(d,l)
+    return "Not a rectangle" if d <= l
+  
+  other_l = Math.sqrt(d**2 - l**2)
+  area = l * other_l
+  
+  area.round(2)
+end
