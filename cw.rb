8732acbad338b8274163f88e070059b0073536e0
kxrxmiq <qwerty1122k@mail.ru>
2025-10-06 11:52:37 +0300

    6

--- cw.rb
+++ cw.rb
@@ -23,3 +23,12 @@ end
 def DNA_strand(dna)
   dna.tr("ACTG", "TGAC")
 end
+
+#Band name generator
+def band_name_generator(name)
+  if name[0] == name[-1]
+    name[0].upcase + name[1..-1] +name[1..-1]
+    else
+    'The '+ name[0].upcase + name[1..-1]
+    end
+end
