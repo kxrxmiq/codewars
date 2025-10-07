78aa898949e00ec7c79bcda480cc31175dfb988a
kxrxmiq <qwerty1122k@mail.ru>
2025-10-07 11:42:25 +0300

    task24

---
+++ task24.rb
@@ -0,0 +1,11 @@
+#greetings with First Name AND Last Name
+class Person
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+  
+  def greet
+    "Hello, #{@first_name} #{@last_name}!"
+  end
+end
